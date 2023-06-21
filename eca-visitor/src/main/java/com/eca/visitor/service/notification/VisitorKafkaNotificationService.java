498707664f10212d5934f00de0bcd55d578c3887
MITALI <mitalihans@gmail.com>
2023-06-21 14:40:45 +0530

    removing unused imports

--- eca-visitor/src/main/java/com/eca/visitor/service/notification/VisitorKafkaNotificationService.java
+++ eca-visitor/src/main/java/com/eca/visitor/service/notification/VisitorKafkaNotificationService.java
@@ -1,7 +1,5 @@
 package com.eca.visitor.service.notification;
 
-import com.eca.visitor.dto.VisitorKafkaMessageDTO;
-import com.eca.visitor.utils.JsonUtils;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,9 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
 @Service
